5dc17b9b4617b00c9550c66c7d5dbbd624764ba6
EmmaGarrr <emmagarcia91992@gmail.com>
2025-07-18 15:28:48 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.mfcnextgen.com:5000',
-  wsUrl: 'wss://api.mfcnextgen.com:5000/ws_api'
+  apiUrl: 'https://api.mfcnextgen.com',
+  wsUrl: 'wss://api.mfcnextgen.com/ws_api'
 };
